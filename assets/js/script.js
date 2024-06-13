142cf4d6859e14fdfeddca0e0556f49b92fe521c
Daniel D <ddunnemann@gmail.com>
2024-06-13 13:33:31 -0500

    I am still trying to wrap my head around where to even begin with this. I have begun tackling the ID generation for the tasks. Thought about a few different ways of doing this and settled on just generating a random number up to 9999. I wanted to try to account for duplicates and give it a minimum length but that was proving more complicated than I was willing to tackle right now. Maybe I'll revisit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,30 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const randomID = Math.floor(Math.random() * 9999);
+    if  (taskList === null) {
+        taskList = [];
+        const randomID = Math.floor(Math.random() * 9999);
+        console.log(typeof(taskList));
+        taskList.push(randomID);
+        localStorage.setItem('tasks', taskList);
+    }
+    else if (taskList.indexOf(randomID) > -1) {
+        const randomID = Math.floor(Math.random() * 9999);
+        taskList.push(randomID);
+        localStorage.setItem('tasks', taskList);
+        return randomID;
+    } else {
+    taskList.push(randomID);
+    localStorage.setItem(taskList);
+    return randomID;
+    }
 }
+console.log(generateTaskId());
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    
 
 }
 
@@ -34,5 +53,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList()
 });
