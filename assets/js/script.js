b2b0559d3b316804ea2d815c989f6e06e7292847
Daniel D <ddunnemann@gmail.com>
2024-06-16 16:00:19 -0500

    Modal is just about where I'd like it to be. I spent way longer than I'd care to admit getting the spacing right of the elements within the Modal Form. I also began putting together the skeleton to handle the actual inputs user puts in the form. I've got them captured in a Task object for now.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,18 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-const taskDueDate = $('#task-due-date');
+const formEl = $('#formModal');
+
+const taskTitleEl = $('#task-title');
+const taskDueDateEl = $('#task-due-date');
+const taskDescEl = $('#task-desc');
+
+const toDoListEl = $('#to-do');
+const inProgressListEl = $('#in-progress');
+const donelistEl = $('#done');
 
 // Todo: create a function to generate a unique task id
-// function generateTaskId() {
+function generateTaskId() {};
 //     const randomID = Math.floor(Math.random() * 9999);
 //     if  (taskList === null) {
 //         taskList = [];
@@ -29,39 +37,70 @@ const taskDueDate = $('#task-due-date');
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    
 
-}
+    
+};
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
-}
+};
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
+    const taskTitle = taskTitleEl.val();
+    const taskDueDate = taskDueDateEl.val();
+    const taskDesc = taskDescEl.val();
+    function Task (title, date, desc) {
+        this.title = taskTitle;
+        this.date = taskDueDate;
+        this.desc = taskDesc;
+    };
+    const newTask = new Task(taskTitle, taskDueDate, taskDesc);
+    console.log(newTask);
+    taskTitleEl.val('');
+    taskDueDateEl.val('');
+    taskDescEl.val('');
 
-}
+};
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
 
-}
+};
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
-}
+};
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Todo: when the page loads, (1.) render the task list, (2.) add event listeners, (3.) make lanes droppable, and (4.) make the due date field a date picker
 $(document).ready(function () {
     renderTaskList()
-$(function () {
-    $(taskDueDate).datepicker({
-        changeMonth: true,
-        changeYear: true,
+
+// Use jQuery syntax .on and 'submit' to handle the formEl submission with corresponding function.
+    formEl.on('submit', handleAddTask);
+
+// (4.) Datepicker functionality
+    $(function () {
+        $(taskDueDateEl).datepicker({
+// This toggles drop down selection lists for month and year. 'false' turns off.
+            changeMonth: true,
+            changeYear: true,
+        });
     });
 
+    $(function() {
+    toDoList = $('#to-do').sortable({
+
+    });
+    inProgressList = $('#in-progress').sortable({
+
+    });
+    donelist = $('#done').sortable({
+
+    });
 });
 
 });
